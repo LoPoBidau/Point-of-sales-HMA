d154143fc660af51dd783d1feb9aa0b31eb87571
Lilopratamaa <lilopratamaa@gmail.com>
2025-08-15 10:43:40 -0700

    Add & Fix : Color and Theme, login page

--- app/src/main/java/com/example/pos_hma/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/pos_hma/ui/login/LoginActivity.kt
@@ -8,11 +8,12 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.pos_hma.R
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.loginActivity)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
